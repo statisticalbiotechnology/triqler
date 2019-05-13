fa2ad5bca476cbdc0c6a57f054a790850a038bc9
patruong <ptruong@kth.se>
2019-05-13 13:17:12 +0200

    .

--- run_test.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-python -m triqler --fold_change_eval 0.8 --out example/iPRG2016.proteins.tsv example/iPRG2016.tsv
-if %errorlevel% == 0 (
-  echo Test succeeded
-) else (
-  echo Test failed
-)
